dfe31766edb8c67ce988635e8d1e0505b53d8625
penkard <penkardbyzainab@gmail.com>
2025-11-27 19:07:46 +0500

    Added Chapter 4 content.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'Konnichiwa',
+  title: 'Welcome',
   tagline: 'japanese are cool',
   favicon: 'img/favicon.ico',
 
@@ -103,7 +103,7 @@ const config: Config = {
           items: [
             {
               label: 'Tutorial',
-              to: '/docs/intro',
+              to: '/docs/',
             },
           ],
         },
